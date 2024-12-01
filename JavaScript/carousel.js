ad52c57fd6d87eeb7a0a51ecbce638616f98c987
EzzoHamdan <111298911+EzzoHamdan@users.noreply.github.com>
2024-12-01 19:53:22 +0300

    Project section update
    1. Now, the viewer can toggle between all or featured projects.
    2. The university course projects now have badges to distinguish them.
    3. Edits on CSS and JS to keep up with the changes.
    4. A new AAU logo has been added.
    5. A temporary fix has been established for timeline title visibility.

--- JavaScript/carousel.js
+++ JavaScript/carousel.js
@@ -3,19 +3,36 @@ const slides = document.querySelectorAll('.slide');
 const totalSlides = slides.length;
 
 function showSlide(n) {
-  currentSlide = (n + totalSlides) % totalSlides; // Looping to the beginning if the end is reached
+  const slides = Array.from(document.querySelectorAll('.slide')).filter(slide => slide.style.display !== 'none');
+  const totalSlides = slides.length;
+  currentSlide = (n + totalSlides) % totalSlides;
   const offset = -100 * currentSlide + '%';
   document.querySelector('.slide-container').style.transform = `translateX(${offset})`;
+
   slides.forEach((slide, index) => {
-    if (index === currentSlide) {
-      slide.style.opacity = 1;
-      slide.style.transform = 'translateY(0)';
-    } else {
-      slide.style.opacity = 0;
-      slide.style.transform = 'translateY(20px)';
-    }
+      if (index === currentSlide) {
+          slide.style.opacity = 1;
+          slide.style.transform = 'translateY(0)';
+      } else {
+          slide.style.opacity = 0;
+          slide.style.transform = 'translateY(20px)';
+      }
   });
 }
+
+function filterProjects(category) {
+  const slides = document.querySelectorAll('.slide');
+  slides.forEach(slide => {
+      const slideCategory = slide.getAttribute('data-category');
+      if (category === 'all' || slideCategory === category) {
+          slide.style.display = 'block';
+      } else {
+          slide.style.display = 'none';
+      }
+  });
+  showSlide(0); // Reset to the first slide
+}
+
 
 function prevSlide() {
   showSlide(currentSlide - 1);
